2c9b529bae32d1ba441a9847db2b3b348458a2cb
KushagraMehta <kushagramehta2000@gmail.com>
2020-03-20 23:35:16 +0530

    20-03-2020 Segment tree & JavaScript

--- Data Sturucture/Segment_Tree.cpp
+++ Data Sturucture/Segment_Tree.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 template <class Type>
 class Segment_Tree
 {
 private:
-    bool min;
+    int _min;
+    Type _max;
     size_t size;
     size_t tree_size;
-    Type *segment;
-    Type *lazy;
+    Type *arr;
+    Type *segment_arr;
 
+    Type _mid(Type low, Type high) { return low + (high - low) / 2; }
+
+    void compare(Type a, Type b)
+    {
+        if (_min == 1)
+            return min(a, b);
+        else if (_min - 1)
+            return (a + b);
+        else
+            return max(a, b);
+    }
     size_t _tree_size(size_t value)
     {
         bool not_2_power = false;
@@ -24,58 +35,59 @@ private:
         } while (value >>= 1);
         return targetlevel - (not_2_power ? 0 : 1);
     }
-    void _build()
+    void build(size_t low, size_t high, size_t position)
     {
-        for (size_t i = size - 1; i > 0; --i)
-            segment[i] = segment[i << 1] + segment[i << 1 | 1];
+        if (high == low)
+        {
+            segment_arr[position] = arr[low];
+            return segment_arr[position];
+        }
+        size_t mid = _mid(low, high);
+        segment_arr[position] = compare(this->build(low, mid, 2 * position + 1), this->build(mid + 1, high, 2 * position + 2));
+        return segment_arr[position];
+    }
+    Type _query(size_t position, size_t child_left, size_t child_right, size_t left, size_t right)
+    {
+        if (left > right)
+            return (_min ? _max : 0);
+
+        if (left == child_left && right == child_right)
+            return segment_arr[position];
+
+        size_t mid = _mid(child_left, child_right);
+        return compare(_query(position * 2 + 1, child_left, mid, left, min(right, mid)),
+                       _query(position * 2 + 2, mid + 1, tr, max(left, mid + 1), right));
     }
 
 public:
-    Segment_Tree(size_t size)
+    Segment_Tree(size_t size, int min = 1, Type max = 2147483647)
     {
+        this->_min = min;
+        this->_max = max;
         this->size = size;
         this->tree_size = _tree_size(size) * 2 - 1;
-        this->segment = new Type[tree_size];
-        this->lazy = new Type[tree_size];
+        this->arr = new Type[size];
+        this->segment_arr = new Type[tree_size];
+        memset(segment_arr, (min ? max : 0), tree_size);
     }
     void input()
     {
-        for (int i = 0; i < size; ++i)
-            cin >> segment[size + i];
-        this->_build();
+        for (size_t i = 0; i < size; i++)
+            cin >> arr[i];
+        this->build(0, size - 1, 0);
     }
-    void answer(size_t low, size_t high);
-    void modify(long long value, size_t index)
+    Type query(size_t left, size_t right) { return _query(0, 0, tree_size - 1, left, right); }
+
+    void update(Type value, size_t low, size_t high = 0)
     {
-        for (segment[index += n] = value; index > 1; index >>= 1)
-            segment[index >> 1] = segment[index] + segment[index ^ 1];
-    }
-    void update(long long value, size_t low, size_t high)
-    {
+        if (low != high && low > high)
+            high = low;
+        // work 2 do
     }
 };
 
-int construct_tree(int *tree_arr, int low, int high, int position)
-{
-    if (high == low)
-    {
-        cin >> tree_arr[position];
-        return tree_arr[position];
-    }
-    int mid = (low + high) / 2;
-    tree_arr[position] = min(construct_tree(tree_arr, low, mid, 2 * position + 1), construct_tree(tree_arr, mid + 1, high, 2 * position + 2));
-    return tree_arr[position];
-}
-
 int main(int argc, char const *argv[])
 {
-    int size, tree_size;
-    printf("Enter the size of array: ");
-    scanf("%intd", &size);
-
-    tree_size = ceil(log2(size)) * 2 - 1;
-    int segment_arr[tree_size] = {INT64_MAX};
-
-    segment_arr[0] = construct_tree(segment_arr, 0, size, 0);
+    // '''' //
     return 0;
 }
